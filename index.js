05757a14c28cb930ab0a209b7e55887991ea630c
yinaw <oliveoilgw@gmail.com>
2014-11-17 22:35:07 -0800

    edit index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-slider'
+  name: 'ember-slider',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/jquery-ui/jquery-ui.js');
+    app.import('bower_components/jquery-ui/ui/slider.js');
+    app.import('bower_components/jquery-ui/themes/base/slider.css');
+    app.import('bower_components/jquery-ui/themes/base/theme.css');
+  }
+
 };
