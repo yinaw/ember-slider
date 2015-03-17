f7a2a5dd8ee982dd2436edfecfd91c157ef875ab
yinaw <oliveoilgw@gmail.com>
2015-03-17 14:35:42 -0700

    remove theme

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ module.exports = {
     app.import('bower_components/jquery-ui/jquery-ui.js');
     app.import('bower_components/jquery-ui/ui/slider.js');
     app.import('bower_components/jquery-ui/themes/base/slider.css');
-    app.import('bower_components/jquery-ui/themes/base/theme.css');
   }
 
 };
