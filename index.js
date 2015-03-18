c90925cf57f13e2e6fa2e5caa6088bbf1d1f7f71
Yuhui Zhang <zyuhui@yahoo-inc.com>
2015-03-18 16:17:02 -0700

    Use lower jQuery ui version and remove theme css

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-slider'
+  name: 'ember-slider',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/jquery-ui/ui/minified/jquery-ui.custom.min.js');
+  }
 };
