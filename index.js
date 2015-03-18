b0d2d2591ed646489ed9da697b693ac79462c981
Yuhui Zhang <zyuhui@yahoo-inc.com>
2015-03-18 00:53:54 -0700

    Dose not import jquery ui

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-slider',
-
-  included: function(app) {
-    this._super.included(app);
-
-    app.import('bower_components/jquery-ui/jquery-ui.js');
-    app.import('bower_components/jquery-ui/ui/slider.js');
-    app.import('bower_components/jquery-ui/themes/base/slider.css');
-  }
-
+  name: 'ember-slider'
 };
